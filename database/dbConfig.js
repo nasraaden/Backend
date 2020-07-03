d45ef958453def315d362f85eb38b887ed870b11
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-07-03 12:47:22 -0700

    this is the last time I am going to try this

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,4 +1,4 @@
 const knex = require('knex');
-const knexConfig = require('.././knexfile');
+const knexConfig = require('../knexfile');
 const enviroment = process.env.DB_ENV || 'development';
 module.exports = knex(knexConfig[enviroment]);
